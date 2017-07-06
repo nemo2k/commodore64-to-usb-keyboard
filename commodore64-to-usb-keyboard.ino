cac8a6c426f51010f7f2d76e47cb9847c5a0f99c
nemo2k <nemo2@protonmail.com>
2017-07-06 23:21:30 +0200

    [nemo2k] restored main code

--- commodore64-to-usb-keyboard.ino
+++ commodore64-to-usb-keyboard.ino
@@ -1,37 +1,122 @@
-uint8_t keymap[192] = {
-//	1, Escape, Control, RunStop, Space, Commodore, Q, 2
-	KEYCODE_1, KEYCODE_ESC, KEYCODE_LEFT_CONTROL, KEYCODE_LEFT_ALT, KEYCODE_SPACE, KEYCODE_LEFT_CONTROL, KEYCODE_Q, KEYCODE_2,
-//	3		W 		A 			LSHIFT 		Z 		S 		E 		4
-	0x20, 	0x1A, 	0x04, 		0x02, 		0x1D, 	0x16, 	0x08, 	0x21,
-//	5 		R 		D 			X 			C 		F 		T 		6
-	0x22, 	0x15, 	0x07, 		0x1B, 		0x06,	0x09, 	0x17, 	0x23,
-//	7 		Y 		G 			V 			B 		H 		U 		8
-	0x24, 	0x1C, 	0x0A, 		0x19, 		0x05, 	0x0B, 	0x18, 	0x25,
-//	9 		I 		J 			N 			M 		K 		O 		0
-	0x26, 	0x0C, 	0x0D, 		0x11, 		0x10, 	0x0E, 	0x12, 	0x27,
-//	+ 		P 		L 			, 			. 		: 		@ 		-
-	0x2D, 	0x13, 	0x0F, 		0x36, 		0x37, 	0x33, 	0x2F, 	0x2E,
-//	£ 		* 		; 			/ 			RSHIFT 	= 		arrowup HOME
-	0x35, 	0x30, 	0x34, 		0x38, 		0xE5, 	0x32, 	0x64, 	0x4A,
-//	DEL 	RETURN 	<- ->		/\ \/		f1 		f3 		f5 		f7
-	0x2A, 	0x28, 	KEYCODE_ARROW_RIGHT, 		KEYCODE_ARROW_DOWN, 		0x3A,	0x3C, 	0x3E, 	0x40
+#include <Arduino.h>
+#include "ProTrinketKeyboard.h"
+#include "commodore64-keys.h"
+
+#define AXISESX 8
+#define AXISESY 8
+
+struct C64
+{
+	byte gpio;
+} C64[AXISESX] = {
+		{0}, 
+		{1},
+		{3},
+		{4},
+		{5},
+		{6},
+		{8},
+		{9}
 };
 
-uint8_t keymapmodifiers[192] = {
-//	1 	  	Escape 	Lft Ctrol 	RunStop 	Space 	Cmmdr 	Q 		2
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	3		W 		A 			LSHIFT 		Z 		S 		E 		4
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	5 		R 		D 			X 			C 		F 		T 		6
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	7 		Y 		G 			V 			B 		H 		U 		8
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	9 		I 		J 			N 			M 		K 		O 		0
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	+ 		P 		L 			, 			. 		: 		@ 		-
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	£ 		* 		; 			/ 			RSHIFT 	= 		arrowup HOME
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0,
-//	DEL 	RETURN 	<- ->		/\ \/		f1 		f3 		f5 		f7
-	0, 		0, 		0, 			0, 			0, 		0, 		0, 		0
+struct SENDERS
+{
+	byte gpio;
+} SENDERS[AXISESY] = {
+		{A5},
+		{A4},
+		{A3},
+		{A2},
+		{A1},
+		{A0},
+		{12},
+		{11}
 };
+
+unsigned long debouncer[64];
+boolean laststate[64];
+boolean state[64];
+
+
+void setup() 
+{
+	for(byte y = 0; y < AXISESY; y++) {
+		pinMode(SENDERS[y].gpio, INPUT);
+	}
+
+	for(byte x = 0; x < AXISESX; x++) {
+		pinMode(C64[x].gpio, INPUT_PULLUP);
+	}
+
+	TrinketKeyboard.begin();
+
+	// Turn on C64 red led
+	pinMode(10, OUTPUT);
+	digitalWrite(10, HIGH);
+}
+
+void loop() {
+	TrinketKeyboard.poll(); // Must be called at least once every 10ms
+	unsigned long t = millis(); // Prevent multiple calls to millis() in loop
+
+	char mod = 0;
+
+	// Check if C= is pressed for control, ctrl on c64 is remapped to tab
+	pinMode(SENDERS[0].gpio, OUTPUT);
+	digitalWrite(SENDERS[0].gpio, LOW);
+	if(digitalRead(C64[5].gpio) == LOW) {
+		mod = 0x01;
+	}
+	digitalWrite(SENDERS[0].gpio, HIGH);
+	pinMode(SENDERS[0].gpio, INPUT);
+
+	// Check if left shift is pressed for shift
+	pinMode(SENDERS[1].gpio, OUTPUT);
+	digitalWrite(SENDERS[1].gpio, LOW);
+	if(digitalRead(C64[3].gpio) == LOW) {
+		mod = 0x02;
+	}
+	digitalWrite(SENDERS[1].gpio, HIGH);
+	pinMode(SENDERS[1].gpio, INPUT);
+
+	// Check if right shift is pressed for alt gr
+	pinMode(SENDERS[6].gpio, OUTPUT);
+	digitalWrite(SENDERS[6].gpio, LOW);
+	if(digitalRead(C64[4].gpio) == LOW) {
+		mod = 0x40;
+	}
+	digitalWrite(SENDERS[6].gpio, HIGH);
+	pinMode(SENDERS[6].gpio, INPUT);
+
+
+	for(byte y = 0; y < AXISESY; y++) {
+		pinMode(SENDERS[y].gpio, OUTPUT);
+		digitalWrite(SENDERS[y].gpio, LOW);
+
+		for(byte x = 0; x < AXISESX; x++) {
+			boolean gpiostate = digitalRead(C64[x].gpio);
+
+			if(gpiostate != laststate[(y*8)+x]) {
+				debouncer[(y*8)+x] = t;
+			}
+
+			if(t - debouncer[(y*8)+x] > 10) {
+				if(laststate[(y*8)+x] != state[(y*8)+x]) {
+					state[(y*8)+x] = laststate[(y*8)+x];
+					if(state[(y*8)+x]) {
+						TrinketKeyboard.pressKey(0,0);
+					} else {
+						if(mod == 0) {
+			        		TrinketKeyboard.pressKey(keymapmodifiers[(y*8)+x], keymap[(y*8)+x]);
+						} else {
+			                TrinketKeyboard.pressKey(mod, keymap[(y*8)+x]);
+						}
+					}
+				}
+			}
+			laststate[(y*8)+x] = gpiostate;
+		}
+		digitalWrite(SENDERS[y].gpio, HIGH);
+		pinMode(SENDERS[y].gpio, INPUT);
+	}
+}
